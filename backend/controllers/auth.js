e7b95f8dfc2f8131eba59724cb13339de9fbe197
neitan-02 <hernandezmorenonathan09@gmail.com>
2026-03-07 23:54:07 -0600

    fix: se corrige el manejo del login y validación de usuario

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,43 +1,42 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt")
 
-exports.login = (req, res) => {
+const login = (req, res) => {
+
     const { email, password } = req.body;
 
     if (!email || !password) {
-        return res.status(400).json({ message: "Correo electronico y la contraseña son requeridos" })
+        return res.status(400).json({ message: "Email y password requeridos" });
     }
 
-    const emailRegex = /\S+@\S+\.\S+/;
+    User.findByEmail(email, (err, user) => {
 
-    if (!emailRegex.test(email)) {
-        return res.status(400).json({ message: "Correo electronico es invalido" });
-    }
-
-    User.finByEmail(email, async (error, user) => {
         if (err) return res.status(500).json(err);
 
         if (!user) {
             return res.status(401).json({ message: "Usuario no encontrado" });
         }
 
-        const validaPassword = await bcrypt.compare(password, user.password);
-
-        if (!validaPassword) {
-            return res.status(401).json({ message: "Contraseña incorrecta" })
+        if (password !== user.password) {
+            return res.status(401).json({ message: "Password incorrecto" });
         }
 
-        req.sesion.user = {
+        req.session.user = {
             id: user.id,
             email: user.email
         };
 
-        res.json({ message: "Inicio de sesión exitoso", user: req.session.user });
+        res.json({ message: "Login exitoso" });
+
+    });
+
+};
+
+const logout = (req, res) => {
+    req.session.destroy(() => {
+        res.json({ message: "Sesión cerrada" });
     });
 };
 
-export default logout = (req, res) => {
-    req.sesion.destroy(() => {
-        res.json({ message: "Sesión Cerrada" });
-    });
-}; 
+module.exports = {
+    login, logout
+};
