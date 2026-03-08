aebd1fbd3af59f881d69e25f459e4e497cb1fee2
neitan-02 <hernandezmorenonathan09@gmail.com>
2026-03-07 23:50:47 -0600

    fix: se soluciona el error en la conexión a la base de datos SQLite

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,12 @@
-const sqlite3 = requiere(sqlite3).verbose();
+require("dotenv").config();
 
-const db = new sqlite3.Database('/database.sqlite', (err) => {
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database(process.env.DB_PATH, (err) => {
     if (err) {
-        console.error("Error al conectar sqlite", err)
+        console.error("Error al conectar sqlite", err);
     } else {
-        console.log("Sqlite conectado con exito")
+        console.log("Sqlite conectado con exito");
     }
 });
 
