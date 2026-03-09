618cb0800f77cc1cc7d7ed219ff5ea2355073034
neitan-02 <hernandezmorenonathan09@gmail.com>
2026-03-08 18:05:20 -0600

    feature: se agrega el manejo de errores para la obtencion de todos los productos o por id

---
+++ backend/controllers/product.js
@@ -0,0 +1,21 @@
+const Product = require('../models/Product');
+
+const product = {
+    listaProductos: (req, res) => {
+        Product.obtenerProductos((err, product) => {
+            if (err) return res.status(500).json({ error: err.message });
+            res.json(product);
+        });
+    },
+
+    detalleProducto: (req, res) => {
+        const id = req.params.pkProduct;
+        Product.obtenerPorId(id, (err, product) => {
+            if (err) return res.status(500).json({ err: err.message });
+            if (!product) return res.status(400).json({ message: 'Producto no encontrado' });
+            res.json(product);
+        });
+    }
+}; 
+
+module.exports = product;
